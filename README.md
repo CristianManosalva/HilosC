8b5904079b45832729b780177a68af7558cabdc9
John Sanabria <john.sanabria@correounivalle.edu.co>
2016-04-26 18:41:15 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Directorio para clase Sistemas Operativos - Abril 26, 2016
+==========================================================
+
+En este repositorio de git estaran los archivos que se implementaron en la clase de sistemas operativos del dia Abril 26, 2016.
---
+++ riemann_erroneo.c
@@ -0,0 +1,52 @@
+/**
+* FILE: riemann_erroneo.c
+* DESCRIPTION:
+* 	Implementacion de la suma de Riemann para calcular el area bajo la
+*	curva. Este codigo esta errado, usted debe hacer los ajustes para que
+*	funcionen correctamente.
+* AUTHOR: John Sanabria - john.sanabria@correounivalle.edu.co
+* LAST REVISED: 14/04/16
+* REFERENCES: http://mathinsight.org/calculating_area_under_curve_riemann_sums
+******************************************************************************/
+
+#include <stdio.h>
+#include <pthread.h>
+#include <math.h>
+
+#define MAXRECT 1000000
+#define MAXTHREAD 10
+#define BASE 0.0
+#define LIMIT 1.0
+
+double length = LIMIT - BASE;
+double numRectxThread = (double)(MAXRECT/MAXTHREAD);
+double base_length = 0;
+double sumTotal = 0;
+
+double function(double x) {
+	return x * x; 
+}
+void* calcular(void *arg) {
+	long id = (long) arg;
+	double lowlimit = id*base_length*numRectxThread;
+
+
+	for (int i = 0; i < numRectxThread; i++) {
+		sumTotal += function(lowlimit + i*base_length) * base_length;
+	}
+	return 0;
+}
+
+int main(int argc, char** argv) {
+	pthread_t threads[MAXTHREAD];
+	long taskids[MAXTHREAD];
+
+	base_length = length/MAXRECT;
+	printf("base length: %lf numRectxThread: %lf\n",base_length, numRectxThread);
+	for (int i = 0; i < MAXTHREAD; i++) {
+		taskids[i] = i;
+		pthread_create(&threads[i], NULL, calcular, (void*)taskids[i]);
+	}
+	printf("Suma total %lf\n",sumTotal);
+	pthread_exit(NULL);
+}
